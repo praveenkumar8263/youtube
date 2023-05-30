495c113fecdfdcaecadb6092204aaf22fd825512
PRAVEEN REDDY <pravenredy2000@gmail.com>
2023-05-30 20:38:06 +0530

    navbar updated

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,31 @@
+import Ytub from "../images/youtube.png";
+import Video from "../images/videocamera_5631.png";
+import Notification from "../images/3643784-bell-notification-notify-reminder-ring_113451.png";
+import "../styles/navber.css";
+function NavBar() {
+  return (
+    <div className="navbar">
+      <div className="log">
+        <img width={120} src={Ytub} alt="youtube" />
+      </div>
 
-function NavBar()
-{
-    return (
-        <div className="navbar">
-            <h1>Navbar</h1>
+      <div className="searchBar">
+        <input type="text" placeholder="search" />
+        
+      </div>
+
+      <div className="userOption">
+        <div className="video">
+          <img width={35} src={Video} alt="" />
         </div>
-    );
+        <div className="video">
+          <img width={35} src={Notification} alt="" />
+        </div>
+        <div className="profile">
+          <p>P</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default NavBar;
